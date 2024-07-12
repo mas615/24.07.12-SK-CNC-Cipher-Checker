e762469029d4e91f217d19fe389a21ed62278241
majun <mas9623@naver.com>
2024-07-12 09:43:41 +0900

    24.07.12 v5

--- cipher.py
+++ cipher.py
@@ -1,11 +1,11 @@
-#© 2024 MaJunYoung akwns615@gmail.com v4
+#© 2024 MaJunYoung akwns615@gmail.com v5
 import subprocess, sys, re
 print("☆ SK C&C Security Team - Cipher Checker 2024 WITH nmap ☆")
+#secures에 안전한 암호화 알고리즘 추가
 secures = ["TLS_ECDHE_ECDSA_WITH_AES_128_GCM_SHA256",
            "TLS_ECDHE_ECDSA_WITH_CHACHA20_POLY1305_SHA256"]
 def cipher_check(command):
     temp = subprocess.check_output(command, shell=True, text=True)
-
     a = temp.split("\n")
     for i in a:
         if " - A" in i or " - B" in i or " - C" in i or " - D" in i or " - F" in i:
@@ -18,12 +18,12 @@ def cipher_check(command):
             print(i)
 
 if len(sys.argv) < 2:
-    print('사용법 : python cipher.py [URL] -p [PORT]')
+    print('!!! How to use !!! : [python cipher.py majun.com] || [python cipher.py majun.com -p 443]')
     sys.exit(1)
 
-#com = "nmap --script ssl-enum-ciphers sk.wd3.myworkdayjobs.com -p 443"
 del sys.argv[0]
 com ="nmap --script ssl-enum-ciphers "
 for i in sys.argv:
     com += i+" "
+
 cipher_check(com)
